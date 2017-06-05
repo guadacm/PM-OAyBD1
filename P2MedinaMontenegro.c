c53bb48be247c386cc8c76a9909f567a45cfadcf
Guada <guada.m@gmail.com>
2017-06-05 10:28:35 -0300

    inio hay_mas deme_otro

--- P2MedinaMontenegro.c
+++ P2MedinaMontenegro.c
@@ -4,6 +4,15 @@
 -- Integrantes: MEDINA, Guadalupe
                 MONTENEGRO, Luis
 
+------------------------------------------ NOTAS y ACLARACIONES: ------------------------------------------
+
+ * M = N/p, N=650, p=0.75, como M tomo el numero primo mas proximo al techo del resultado de la division
+   M = techo(650/0.75)= 867, entonces M = 877.
+
+-- RAL:
+ * "*" celda virgen (nunca usada).
+ * "#" celda libre (antes ocupada, ahora libre).
+ * Inicializamos el RAL poniendo "*" en el campo Cod_socio.
 */
 
 #include <stdio.h>
@@ -12,21 +21,30 @@
 
 #define P 0.75  //Ro
 #define N 650
-#define M 877   //M = N/P = 866.66. El Nº primo siguiente a M es 877
+//#define M 877   //M = N/P = 866.66. El Nº primo siguiente a M es 877
+#define M 11   // M=8/0.75= 10.66 -> M=11
 
 typedef struct
 {
-    char Cod_Socio[8];
-    char Cod_copia[8];
-    char P_fecha_prestamo[10];
-    char P_fecha_dev[10];
+    char Cod_socio[7];
+    char Cod_copia[7];
+    char P_fecha_prestamo[9];
+    char P_fecha_dev[9];
 } Prestamo;
 
-Prestamo RAL[M];
-int cant_Prestamos = 0;
+
 
 /* VARIABLES */
 int opcion = -1;
+Prestamo RAL[M]; //Definicion del Rebalse Abierto Lineal, de dimensión M.
+int cant_Prestamos = 0; // Cantidad de elementos almacenados en el RAL.
+
+char x_int[7];
+int i_int;
+int hash; // Para almacenar el valor devuelto por la funcion hashing()
+int consultas;
+int celda_libre;
+Prestamo buscar;
 /* END VARIABLES */
 
 /* PROTOTYPE */
@@ -34,18 +52,15 @@ void encabezado();
 void init();
 int hashing (char[]);
 int alta(Prestamo);
-int localizar(char[], char[], int*, int*);
+//int localizar(char[], char[], int*, int*);
 void mostrar();
 void memorizacion_previa();
-/* END PROTOTYPE */
 
-void encabezado()
-{
-    system("cls");
-    printf("\n **************************************************\n"
-           " *** ORGANIZACION DE ARCHIVOS Y BASE DE DATOS I ***\n"
-           " **************************************************\n");
-}
+int localizar(char codSoc[],char codCop[]);
+void Inicio(char cod_s[]);
+int Hay_mas();
+void Deme_otro();
+/* END PROTOTYPE */
 
 int main()
 {
@@ -76,7 +91,12 @@ int main()
 
         case 5:
             printf("\n[4] Mostrar Estructura\n");
-            if (cant_Prestamos==0) printf("\n\t Estructura VACIA...\n");
+            if (cant_Prestamos == 0)
+            {
+                encabezado();
+                printf("\n\t Estructura VACIA...\n\n");
+            }
+
             else mostrar();
             system("pause");
             break;
@@ -93,14 +113,22 @@ int main()
     return 0;
 }
 
+void encabezado()
+{
+    system("cls");
+    printf("\n **************************************************\n"
+           " *** ORGANIZACION DE ARCHIVOS Y BASE DE DATOS I ***\n"
+           " **************************************************\n");
+}
+
 void init()
 {
     int i;
     for(i = 0; i < M; i++)      // Inicializo el RAL con la marca de celda virgen
-        strcpy(RAL[i].Cod_Socio, "*");
+        strcpy(RAL[i].Cod_socio, "*");
 }
 
-int localizar(char codSoc[],char codCop[],int *h, int *pos) // en h guardo el valor que me da la funcion hashing, en pos, la posicion donde debe/deberia estar el elemento que busco
+/*int localizar(char codSoc[],char codCop[],int *h, int *pos) // en h guardo el valor que me da la funcion hashing, en pos, la posicion donde debe/deberia estar el elemento que busco
 {
     *h = hashing(codSoc);
     *pos = *h;
@@ -108,18 +136,18 @@ int localizar(char codSoc[],char codCop[],int *h, int *pos) // en h guardo el va
     {
         int i = *h;
         int f = 0; // si esta en 1 me dice que ya guardo en pos una celda libre
-        while((strcmp(RAL[i].Cod_Socio, codSoc)!= 0 || strcmp(RAL[i].Cod_copia, codCop)!= 0) && strcmp(RAL[i].Cod_Socio, "*")!= 0)
+        while((strcmp(RAL[i].Cod_socio, codSoc)!= 0 || strcmp(RAL[i].Cod_copia, codCop)!= 0) && strcmp(RAL[i].Cod_socio, "*")!= 0)
         {
-            if(strcmp(RAL[i].Cod_Socio, "#") == 0 && f == 0)
+            if(strcmp(RAL[i].Cod_socio, "#") == 0 && f == 0)
             {
                 *pos = i;
                 f = 1;
             }
             i = (i + 1) % M;
         }
-        if(strcmp(RAL[i].Cod_Socio, "*") == 0 && f == 0)
+        if(strcmp(RAL[i].Cod_socio, "*") == 0 && f == 0)
             *pos = i;
-        if(strcmp(RAL[i].Cod_Socio, codSoc)== 0 && strcmp(RAL[i].Cod_copia, codCop)== 0)
+        if(strcmp(RAL[i].Cod_socio, codSoc)== 0 && strcmp(RAL[i].Cod_copia, codCop)== 0)
         {
             *pos = i;
             return 1;
@@ -134,6 +162,54 @@ int localizar(char codSoc[],char codCop[],int *h, int *pos) // en h guardo el va
     {
         return 0;
     }
+}*/
+
+int localizar(char codSoc[],char codCop[])
+{
+    Inicio(codSoc);
+    while (Hay_mas() != 0)
+    {
+        Deme_otro();
+        if (strcmp(buscar.Cod_copia, codCop) == 0) {return 1;}
+
+    }
+    return 0;
+}
+
+void Inicio(char cod_s[])
+{
+    strcpy(x_int, cod_s);
+    i_int = hashing(x_int);
+    celda_libre = -1;
+}
+
+int Hay_mas() // Devuelve 1 si encontro una tupla con el codigo buscado, sino devuelve 0.
+{
+    while (strcmp(RAL[i_int].Cod_socio, x_int) != 0 && strcmp(RAL[i_int].Cod_socio, "*") != 0 && consultas < M)
+    {
+        if (strcmp(RAL[i_int].Cod_socio, "#") == 0 && celda_libre == -1) {celda_libre = i_int;}
+        i_int = (i_int + 1) % M;
+        consultas++;
+    }
+    if(strcmp(RAL[i_int].Cod_socio, x_int) == 0)
+    {
+        celda_libre = i_int
+        return 1;
+    }
+    else
+    {
+        if(strcmp(RAL[i_int].Cod_socio, "*") == 0 && celda_libre == -1) {celda_libre = i_int;}
+        return 0;
+    }
+    //if(consultas == M-1) {return 0;}    //CORRECCION
+}
+
+void Deme_otro() // Devuelve la tupla encontrada en la variale buscar
+{
+    strcpy(buscar.Cod_socio, x_int);
+    strcpy(buscar.Cod_copia, RAL[i_int].Cod_copia);
+    strcpy(buscar.P_fecha_prestamo, RAL[i_int].P_fecha_prestamo);
+    strcpy(buscar.P_fecha_dev, RAL[i_int].P_fecha_dev);
 }
 
 int hashing (char codSoc[])
@@ -151,9 +227,11 @@ int alta(Prestamo nuevo)
     int h, pos;
     if(cant_Prestamos < M)
     {
-        if(localizar(nuevo.Cod_Socio,nuevo.Cod_copia, &h, &pos) == 0)
+        //if(localizar(nuevo.Cod_socio,nuevo.Cod_copia, &h, &pos) == 0)
+        if(localizar(nuevo.Cod_socio,nuevo.Cod_copia) == 0)
         {
-            RAL[pos] = nuevo;
+            //RAL[pos] = nuevo;
+            RAL[celda_libre] = nuevo;
             cant_Prestamos++;
             return 1;
         }
@@ -162,13 +240,13 @@ int alta(Prestamo nuevo)
     else return 0;
 }
 
-void imprimirArt(Prestamo prestamo)
+void mostrar_tupla(Prestamo prestamo)
 {
-    printf("\n Codigo Socio: \t\t%s"
-           "\n Codigo Copia: \t\t%s"
-           "\n Fecha Prestamo: \t%s"
-           "\n Fecha Devolucion: \t%s\n",
-           prestamo.Cod_Socio,
+    printf("\n\t Codigo Socio: \t\t%s"
+           "\n\t Codigo Copia: \t\t%s"
+           "\n\t Fecha Prestamo: \t%s"
+           "\n\t Fecha Devolucion: \t%s\n",
+           prestamo.Cod_socio,
            prestamo.Cod_copia,
            prestamo.P_fecha_prestamo,
            prestamo.P_fecha_dev);
@@ -177,19 +255,20 @@ void imprimirArt(Prestamo prestamo)
 void mostrar()
 {
     int i, j = 1;    // j es para limitar las celdas que se muestran
+    encabezado();
     printf("\n\t----------LISTA DE PRESTAMOS----------\n"
            "\n\t     Cantidad de prestamos: %d\n", cant_Prestamos);
     for(i = 0; i < M; i++)
     {
         printf("\n Posicion: %d", i);
-        if(strcmp(RAL[i].Cod_Socio, "*") == 0) // Celdaa virgen
+        if(strcmp(RAL[i].Cod_socio, "*") == 0) // Celdaa virgen
         {
             printf("\t * Celda nunca usada\n");
             j++;
         }
         else
         {
-            if(strcmp(RAL[i].Cod_Socio, "#") == 0) // Celda libre
+            if(strcmp(RAL[i].Cod_socio, "#") == 0) // Celda libre
             {
                 printf("\t # Celda libre\n");
                 j++;
@@ -198,7 +277,7 @@ void mostrar()
             else        // Celda ocupada
             {
                 printf("\t - Celda ocupada por:");
-                imprimirArt(RAL[i]);
+                mostrar_tupla(RAL[i]);
                 j++;    //Limitador de articulos a mostrar
             }
         }
@@ -222,13 +301,15 @@ void memorizacion_previa()
     {
         while(!(feof(fp)))
         {
-            fscanf(fp," %[^\n]",nuevo.Cod_Socio);
-            strupr(nuevo.Cod_Socio);
+            fscanf(fp," %[^\n]",nuevo.Cod_socio);
+            strupr(nuevo.Cod_socio);
             fscanf(fp," %[^\n]",nuevo.Cod_copia);
+            strupr(nuevo.Cod_copia);
             fscanf(fp," %[^\n]",nuevo.P_fecha_prestamo);
             fscanf(fp," %[^\n]",nuevo.P_fecha_dev);
             alta(nuevo);
         }
+        encabezado();
         printf("\n\nLa memorizacion se ha llevado a cabo\n\n");
     }
     fclose(fp);
